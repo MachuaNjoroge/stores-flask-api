60520098e47ddd569ecce393da95df9628cbfae5
Kelvin Njoroge <knjoroge@IPSL.CO.KE>
2024-05-15 06:02:46 +0300

    Adding many to many relationship using secondary table

--- schemas.py
+++ schemas.py
@@ -15,10 +15,6 @@ class PlainTagSchema(Schema):
     id = fields.Int()
     name = fields.Str()
 
-class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)  
-    store = fields.Nested(PlainStoreSchema(), dump_only=True) 
-
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
@@ -30,6 +26,11 @@ class StoreUpdateSchema(Schema):
     id = fields.Int()
 
 
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)  
+    store = fields.Nested(PlainStoreSchema(), dump_only=True) 
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
@@ -38,4 +39,12 @@ class StoreSchema(PlainStoreSchema):
 
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)  
-    store = fields.Nested(PlainStoreSchema(), dump_only=True) 
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+
--- test.py
+++ test.py
@@ -1,9 +1,27 @@
-x = 1
+import functools
 
-def myfunc():
-  global x
-  x += 1
-  print(x)
+user = {"username": "jose", "level": "admin"}
+
+
+def make_secure(level):
+  def decorator(func):
+      @functools.wraps(func)
+      def secure_function(*args, **kwargs):
+          if user["level"] == level:
+              return func(*args, **kwargs)
+          else:
+              return f"No admin permissions for {user["username"]}"
+      return secure_function
+  return decorator
   
 
-myfunc()
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 1234"
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+print(get_admin_password())
+
